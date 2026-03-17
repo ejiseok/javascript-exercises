54fd5ae1cd994775a33650f7022bada537146e00
ejiseok <jiseoklee0502@gmail.com>
2026-03-17 19:15:55 +0900

    Solve Exercise foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  
+  const cleanedStr = str
+    .toLowerCase()
+    .split("")
+    .filter(ch => validChars.includes(ch))
+    .join("");
 
+  const reversedStr = cleanedStr.split("").reverse().join("");
+
+  return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
