834afbaa67f1988aec630437e74d59f3bf6e453b
ejiseok <jiseoklee0502@gmail.com>
2026-03-17 19:47:03 +0900

    Solve Exercise foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    return new Date().getFullYear() - birth;
+  }
+  return death - birth;
+};
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
